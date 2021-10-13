3cad180f135a12a845abb026263d14d955411c31
TeymoorianSisian <89603308+TeymoorianSisian@users.noreply.github.com>
2021-10-13 16:17:01 -0700

    added show_menu and search_serial_num

--- address_book_menu.h
+++ address_book_menu.h
@@ -12,7 +12,7 @@
 void menu_header(const char *str);
 void main_menu(void);
 void show_menu(void);
-int search_sr_num(AddressBook* address_book, char* search_str, int sr_no);
+int search_serial_num(AddressBook* address_book, char* search_str, int sr_no);
 Status menu(AddressBook *address_book);
 Status save_prompt(AddressBook *address_book);
 Status add_contacts(AddressBook *address_book);
